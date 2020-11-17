f4f15a3542fbe03aa915d49694caaf946eff1ee7
Asen <asengreenfire@gmail.com>
2020-11-17 11:47:40 +0700

    PyPI

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="human_generate-Asen", # Replace with your own username
+    version="0.0.5",
+    author="Asen",
+    author_email="asengreenfire@gmail.com",
+    description="Create fullname and birthday",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Asengreen/human_generate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
